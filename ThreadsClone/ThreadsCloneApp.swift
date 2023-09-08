e2c72977ce22b0bd09a3712ada0fe4232402fa59
Ayman-Naim <mnameno_2012@yahoo.com>
2023-09-08 22:41:35 +0300

    Initial Commit

---
+++ ThreadsClone/ThreadsCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsCloneApp.swift
+//  ThreadsClone
+//
+//  Created by ayman on 08/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
